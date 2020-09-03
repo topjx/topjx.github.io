40b2a5c2c99c9ee2e9fb7dd45702d97437387dca
lijunxi <1178860884@qq.com>
2020-09-03 18:11:55 +0800

    no message

--- chy/js/config.js
+++
@@ -1,6 +0,0 @@
-window.global_config = {
-  //BASE_URL: 'http://121.40.182.24:8251',//ip方式
-  //BASE_URL: 'https://10.7.1.213/webapi',//客户环境
-  BASE_URL: 'https://iih.mmbbsay.com/webapi',//公司环境域名方式
-  //BASE_URL: 'https://iih.xjtlu.edu.cn/webapi',//客户环境域名方式
-}
--- manage/js/config.js
+++
@@ -1,5 +0,0 @@
-window.global_config = {
-  BASE_URL:'https://iih.mmbbsay.com/webapi',
-  // BASE_URL: 'http://121.40.182.24:8251',
-  //BASE_URL: 'http://10.7.1.213/webapi',
-}
